6ae144f5ecee7df0f81ca0dda8e8305606c25d74
BRIAN WIGGINS <64331187+Bigwiggz@users.noreply.github.com>
2021-06-24 22:16:37 -0400

    Add files via upload

---
+++ Javascript/GoogelAutoComplete/googleMapsAutocompleteLatLong.js
@@ -0,0 +1,22 @@
+/*
+GOOGLE GEOCODE
+*/
+
+//Initialize Geocoder
+geocoder = new google.maps.Geocoder();
+
+////////////////////////////////////
+//Google Autocomplete and retrieve Lat and Long
+////////////////////////////////////
+function initializeAutoComplete() {
+          let input = document.getElementById('searchTextField');
+          let autocomplete = new google.maps.places.Autocomplete(input);
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                let place = autocomplete.getPlace();
+                document.getElementById('city2').value = place.name;
+                document.getElementById('cityLat').value = place.geometry.location.lat();
+                document.getElementById('cityLng').value = place.geometry.location.lng();
+				
+            });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
